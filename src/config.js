484c95ffd5658f41a17ff03392592b330e15ef7c
Lanil Marasinghe <lanil@bitbucket.org>
2020-04-12 23:28:43 +0530

    Setup client

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "notes-app-upload-lanil"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://zscrmjkzuf.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_tZOSSDNFl",
+        APP_CLIENT_ID: "63ddl7m1nbhai442htl068i1jt",
+        IDENTITY_POOL_ID: "us-east-1:6cfd87a2-e6c9-4938-8011-9cf752a118ac"
+    }
+};
